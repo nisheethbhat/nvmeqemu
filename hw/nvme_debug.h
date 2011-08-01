a42af6b9026f683d3930cc986634ad3baadeff69
Nisheeth Bhat <nisheeth.bhat@intel.com>
2011-08-01 09:42:38 -0600

    Closed the cosmetic review comment of #define : http://dcgshare.lm.intel.com/reviews/r/188/

--- hw/nvme_debug.h
+++ hw/nvme_debug.h
@@ -3,14 +3,14 @@
 
 
 #define LOG_NORM(fmt, ...)    \
-    printf("%s: " fmt "\n", LEVEL, ## __VA_ARGS__);
+    printf("%s: " fmt "\n", LEVEL, ## __VA_ARGS__)
 #define LOG_ERR(fmt, ...)    \
     printf("%s-ERR:%s:%d: " fmt "\n", LEVEL, __FILE__, \
-        __LINE__, ## __VA_ARGS__);
+        __LINE__, ## __VA_ARGS__)
 #ifdef DEBUG
 #define LOG_DBG(fmt, ...)    \
     printf("%s-DBG:%s:%d: " fmt "\n", LEVEL, __FILE__, \
-        __LINE__, ## __VA_ARGS__);
+        __LINE__, ## __VA_ARGS__)
 #else
-#define LOG_DBG(fmt, ...)    ;
+#define LOG_DBG(fmt, ...)
 #endif
